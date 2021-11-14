112e70f0290af622ef04ecfff5165752804a4232
Glitch (chipped-whispering-ziconium) <none>
2021-11-14 00:16:15 +0000

    ⏱🌛 Checkpoint ./Commands/Message/Information/status.js:22439621/423

--- Commands/Message/Information/status.js
+++ Commands/Message/Information/status.js
@@ -1,7 +1,7 @@
 // ─────────────────────────────────── || MODULE || ─────────────────────────────────── //
 
 
-const { Discord, version } = require("discord.js");
+const { MessageEmbed, version } = require("discord.js");
 const moment = require("moment");
 require("moment-duration-format");
 const os = require('os')
@@ -27,37 +27,36 @@ module.exports = {
 // ─────────────────────────────────── || SYSTEM || ─────────────────────────────────── //
 
 
-    async execute(client, message, args) {
+    async execute(client, message, args, color) {
 
 
         const duration1 = moment.duration(message.client.uptime).format(" D [days], H [hrs], m [mins], s [secs]");
         const cpu = await si.cpu();
 
-        const embed = new Discord.MessageEmbed()
-            .setColor(message.client.color)
+        const embed = new MessageEmbed()
+            .setColor(color)
             .setThumbnail(message.client.user.displayAvatarURL())
-            .setFooter(`Request by: ${message.author.tag}`, message.author.displayAvatarURL())
-            .setTitle(`ℹ️ Status`)
-            .setDescription(`**= STATISTICS =**
-**• Servers** : ${message.client.guilds.cache.size.toLocaleString()}
-**• Channels** : ${message.client.channels.cache.size.toLocaleString()}
-**• Users** : ${message.client.users.cache.size.toLocaleString()}
-**• Discord.js** : v${version}
-**• Node** : ${process.version}
-**• Distube** : v${message.client.distube.version}
+            .setFooter(`Request by ${message.author.tag}`, message.author.displayAvatarURL())
+            .setDescription(`
 
-**= SYSTEM =**
-**• Platfrom** : ${os.type}
-**• Uptime** : ${duration1}
-**• CPU** :
-> **• Cores** : ${cpu.cores}
-> **• Model** : ${os.cpus()[0].model} 
-> **• Speed** : ${os.cpus()[0].speed} MHz
-**• MEMORY** :
-> **• Total Memory** : ${(os.totalmem() / 1024 / 1024).toFixed(2)} Mbps
-> **• Free Memory** : ${(os.freemem() / 1024 / 1024).toFixed(2)} Mbps
-> **• Heap Total** : ${(process.memoryUsage().heapTotal / 1024 / 1024).toFixed(2)} Mbps
-> **• Heap Usage** : ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} Mbps
+**・ Servers** : ${message.client.guilds.cache.size.toLocaleString()}
+**・ Channels** : ${message.client.channels.cache.size.toLocaleString()}
+**・ Users** : ${message.client.users.cache.size.toLocaleString()}
+**・ Discord.js** : v${version}
+**・ Node** : ${process.version}
+
+
+**・ Platfrom** : ${os.type}
+**・ Uptime** : ${duration1}
+**・ CPU** :
+> **・ Cores** : ${cpu.cores}
+> **・ Model** : ${os.cpus()[0].model} 
+> **・ Speed** : ${os.cpus()[0].speed} MHz
+**・ MEMORY** :
+> **・ Total Memory** : ${(os.totalmem() / 1024 / 1024).toFixed(2)} Mbps
+> **・ Free Memory** : ${(os.freemem() / 1024 / 1024).toFixed(2)} Mbps
+> **・ Heap Total** : ${(process.memoryUsage().heapTotal / 1024 / 1024).toFixed(2)} Mbps
+> **・ Heap Usage** : ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} Mbps
 `);
 
         message.channel.send({ embeds: [embed] });
@@ -67,3 +66,6 @@ module.exports = {
 
 
 }
+
+
+// **・ Distube** : v${message.client.distube.version}
