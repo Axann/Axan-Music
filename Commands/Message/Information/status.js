4f8d94e113ec98521525391db9529c9abf6291ec
Glitch (chipped-whispering-ziconium) <none>
2021-11-14 00:39:21 +0000

    🚄🛥 Checkpoint ./Commands/Message/Music/autoplay.js:22439621/7:38706790/2453 ./Commands/Message/Information/status.js:22439621/278

--- Commands/Message/Information/status.js
+++ Commands/Message/Information/status.js
@@ -35,11 +35,12 @@ module.exports = {
 
         const embed = new MessageEmbed()
             .setColor(color)
-            .setThumbnail(message.client.user.displayAvatarURL())
-            .setFooter(`Request by ${message.author.tag}`, message.author.displayAvatarURL())
+            .setTitle(`BOT INFO`)
+            .setThumbnail(client.user.displayAvatarURL())
+            .setFooter(`Requested by ${message.author.tag}`, message.author.displayAvatarURL())
    
             .addField('Info', `\`\`\`yaml\n- Owner      : zicc#1101 & ahsan#3464\n- Version    : v1.0\n- Server     : ${message.client.guilds.cache.size.toLocaleString()}\n- Channels   : ${message.client.channels.cache.size.toLocaleString()}\n- User       : ${message.client.users.cache.size.toLocaleString()}\`\`\`\n`)
-            .addField('Bot Status', `\`\`\`yaml\n- Processor  : ${os.cpus()[0].model}\n- Cores    : ${cpu.cores}\n- Speed    : ${os.cpus()[0].speed} MHz\n- Platform   : ${os.type}\n- Node.js    : ${process.version}\n- Discord.js : v${version}\`\`\`\n`)
+            .addField('Bot Status', `\`\`\`yaml\n- Processor  : ${os.cpus()[0].model}\n- Cores      : ${cpu.cores}\n- Speed      : ${os.cpus()[0].speed} MHz\n- Platform   : ${os.type}\n- Node.js    : ${process.version}\n- Discord.js : v${version}\`\`\`\n`)
             .addField('System Info', `\`\`\`yaml\n- Memory     : ${(process.memoryUsage().rss / 1024 / 1024).toFixed(2)} MB RSS - ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB Heap\n- Node       : ${(process.cpuUsage().user / 1024 / 1024).toFixed(2)} %\n- System     : ${(process.cpuUsage().system / 1024 / 1024).toFixed(2)} %\n- Ping       : ${client.ws.ping} ms\n- Uptime     : ${duration1}\`\`\`\n`)
       
         message.channel.send({ embeds: [embed] });
--- Commands/Message/Music/autoplay.js
+++ Commands/Message/Music/autoplay.js
@@ -1,10 +1,14 @@
+// ─────────────────────────────────── || MODULE NGENTOT || ─────────────────────────────────── //
 
 
 const { MessageEmbed } = require("discord.js");
 
 
+// ─────────────────────────────────── || EXPORT || ─────────────────────────────────── //
+
 
 module.exports = {
+  
     name: "autoplay",
     category: "Music",
     aliases: [ "ap" ],
@@ -15,31 +19,44 @@ module.exports = {
     memberPermissions: [],
     botPermissions: [ "SEND_MESSAGES" ],
     owner: false,
-  
-  
-    async execute(client, message, args) {
+    async execute(client, message, args, color) {
       
+      
+// ─────────────────────────────────── || MISAL GAK NAK VC || ─────────────────────────────────── //
+
+
+      const embedgagal = new MessageEmbed()
+              .setColor(color)
       
         const memberVC = message.member.voice.channel;
       
-
-      
-      
-      
-      
-        if (!memberVC) return message.channel.send(`❌ | You must be in a voice channel!`);
+        if (!memberVC) {
+          embedgagal.setDescription(`❌ | You must be in a voice channel!`)
+          return message.channel.send({ embeds: [embedgagal] });
+        } 
 
         const clientVC = message.guild.me.voice.channel;
-        if (!clientVC) return message.channel.send(`❌ | I'm not on any voice channel!`);
+      
+        if (!clientVC) {
+          embedgagal.setDescription(`<:N_:848429469688397854> | I'm not on any voice channel!`)
+          return message.channel.send({ embeds: [embedgagal] });
+        }
 
-        if (memberVC !== clientVC) return message.channel.send(`❌ | You must be in the same channel as ${message.client.user}!`);
+        if (memberVC !== clientVC) return message.channel.send(`<:N_:848429469688397854> | You must be in the same channel as ${message.client.user}!`);
 
         const queue = message.client.distube.getQueue(message);
-        if (!queue) return message.channel.send(`❌ | There is no music playing!`);
+      
+        if (!queue){
+          embedgagal.setDescription(`<:N_:848429469688397854> | There is no music playing!`)
+          return message.channel.send({ embeds: [embedgagal] });
+        };
+
+      
+// ─────────────────────────────────── || MISAL SUKSES || ─────────────────────────────────── //
+
 
         const embed = new MessageEmbed()
-                .setColor(message.client.color)
-                .setFooter(`Request by ${message.author.tag}`, message.author.displayAvatarURL());
+                .setColor(color)
 
         if (!queue.autoplay) {
             message.client.distube.toggleAutoplay(message);
